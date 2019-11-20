df4a3e4399c353171c9237202d5a5e0fbfd840d4
Bhupesh-V <varshneybhupesh@gmail.com>
2019-11-20 13:01:05 +0530

    New README format - Add new til

---
+++ Miscellaneous/clean-commit.md
@@ -0,0 +1,49 @@
+# Writing Cleaner Commits - Template
+
+Writing cleaner commits is hard, so I use this template which makes me a pro 😅
+
+```text
+# If applied, this commit will...
+# [Add/Fix/Remove/Update/Refactor/Document] [issue #id] [summary]
+
+
+# Why is it necessary? (Bug fix, feature, improvements?)
+-
+# How does the change address the issue? 
+-
+# What side effects does this change have?
+-
+
+```
+
+##### OR
+
+```text
+# If applied, this commit will...
+# [Add/Fix/Remove/Update/Refactor/Document]
+
+# Reference any issue number here
+- This fixes #454
+# Why is it necessary? (Bug fix, feature, improvements?)
+-
+# How does the change address the issue? 
+-
+```
+
+## How ?
+You have to configure Git to use the above template file (for example, `.gitmessage` in your home directory), then create the template file by running.
+
+```bash
+git config --global commit.template ~/.gitmessage
+subl ~/.gitmessage
+```
+
+This will invoke sublime with the tempplate (use `code` if you use VSCode)Now copy paste the above template, hit save and your are done.
+
+Now when commitng changes instead of using `git commit -m ""`, Use `git commit` this will invoke the commit template which you already set.
+
+
+### Resources
+
+- [How to Write a Git Commit Message](https://chris.beams.io/posts/git-commit/)
+- [Git commit practices your future self will thank you for](https://victoria.dev/blog/git-commit-practices-your-future-self-will-thank-you-for/)
--- createTIL.py
+++ createTIL.py
@@ -6,26 +6,54 @@
 import os
 import json
 
-HEADER = '''# TIL
+HEADER = '''
+<h1 align="center">TIL</h1>
+<p align="center">
+  <img src="https://repository-images.githubusercontent.com/192476462/fdd6ce80-0b94-11ea-8b50-812ee66b0599" />
+  <img alt="TILs Count" src="https://img.shields.io/badge/dynamic/json.svg?color=orange&label=TILs&query=count&url=https%3A%2F%2Fraw.githubusercontent.com%2FBhupesh-V%2Ftil%2Fmaster%2Fcount.json">
+  <a href="https://github.com/Bhupesh-V/til/blob/master/LICENSE">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/Bhupesh-V/til" target="_blank" />
+  </a>
+  <img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/m/Bhupesh-V/til">
+  <a href="https://bhupesh.codes/til/">
+    <img alt="Website Status" src="https://img.shields.io/website?down_color=red&down_message=offline&up_color=orange&up_message=online&url=https%3A%2F%2Fbhupesh.codes%2Ftil%2F" />
+  </a>
+  <a href="https://twitter.com/bhupeshimself">
+    <img alt="Twitter: Bhupesh Varshney" src="https://img.shields.io/twitter/follow/bhupeshimself.svg?style=social" target="_blank" />
+  </a>
+</p>
 
-> Today I Learned
-
+> Today I Learned.
 A collection of concise write-ups on small things I learn across a variety of 
 languages and technologies.
 
-![til](https://img.shields.io/badge/dynamic/json.svg?color=orange&label=TILs&query=count&url=https%3A%2F%2Fraw.githubusercontent.com%2FBhupesh-V%2Ftil%2Fmaster%2Fcount.json)
 
 '''
 
 FOOTER = '''## Usage
+
 See [USAGE.md](https://github.com/Bhupesh-V/til/blob/master/USAGE.md) to know how I use this repository.
 
 ## Author
 
-👥 **Bhupesh Varshney**
+👤 **Bhupesh Varshney**
 
-- Twitter: [@bhupeshimself](https://twitter.com/bhupeshimself)
-- Github: [@Bhupesh-V](https://github.com/Bhupesh-V)
+- Twitter : [@bhupeshimself](https://twitter.com/bhupeshimself)
+- DEV : [bhupesh](https://dev.to/bhupesh)
+
+
+## ☺️ Show your support
+
+Support me by giving a ⭐️ if this project helped you! or just [![Twitter URL](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Fgithub.com%2FBhupesh-V%2Ftil%2F)](https://twitter.com/intent/tweet?url=https://github.com/Bhupesh-V/til&text=til%20via%20@bhupeshimself)
+
+<a href="https://www.patreon.com/bhupesh">
+  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## 📝 License
+
+Copyright © 2019 [Bhupesh Varshney](https://github.com/Bhupesh-V).<br />
+This project is [MIT](https://github.com/Bhupesh-V/til/blob/master/LICENSE) licensed.
 
 ## About
 
